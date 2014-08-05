4025fac7d0b44da48a10fb7eb5dc6987a13a7630
Eliot Piering <eliot@radialogica.com>
2014-08-05 18:34:37 -0500

    wip

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   corsWithCredentials: true,
-  host: 'http://192.168.200.198:1337'
+  host: 'http://safe-temple-8585.herokuapp.com'
 });
